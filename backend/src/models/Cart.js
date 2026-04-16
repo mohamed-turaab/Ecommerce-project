d6aa70ca6837249b39d2b5fad2ec5d8cb846a9c0
mohamed-turaab <mohamedturaab1991@gmail.com>
2026-04-16 21:44:37 +0300

    feat: add cart model

---
+++ backend/src/models/Cart.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+  },
+  { timestamps: true }
+);
+
+cartSchema.virtual('totalPrice').get(function () {
+  return this.items.reduce((total, item) => total + item.price * item.quantity, 0);
+});
+
+cartSchema.set('toJSON', { virtuals: true });
+cartSchema.set('toObject', { virtuals: true });
+
+module.exports = mongoose.model('Cart', cartSchema);
